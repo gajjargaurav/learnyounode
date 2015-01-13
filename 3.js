34231a48f3e26ade5020b5c2d9777c17d8dff167
gajjargaurav <gajjargaurav@gmail.com>
2015-01-13 10:41:33 +0000

    improving the answer

--- 3.js
+++ 3.js
@@ -1,5 +1,3 @@
 var fs = require('fs');
 var buff = fs.readFileSync(process.argv[2]);
-var str = buff.toString();
-var countArray = str.split('\n');
-console.log(countArray.length - 1);
+console.log(buff.toString().split('\n').length - 1);
