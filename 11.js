9403ce967b31bf849340da0f6d873e130e217df6
gajjargaurav <gajjargaurav@gmail.com>
2015-01-14 11:51:55 +0000

    exercise 11 complete

---
+++ 11.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function(req,res){
+  var stream = fs.createReadStream(process.argv[3]);
+  stream.on('error', function(err) {
+    res.end(err);
+  });
+  stream.pipe(res);
+})
+
+server.listen(process.argv[2]);
